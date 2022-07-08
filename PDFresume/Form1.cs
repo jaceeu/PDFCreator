ea105b71de43327d9ad3073fd3aea7f72e1d5102
Jhan Carlo Vasquez <jhancarlo.vasquez@tup.edu.ph>
2022-07-08 16:54:24 +0800

    json convert

--- PDFresume/Form1.cs
+++ PDFresume/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace PDFresume
 {
@@ -27,6 +28,8 @@ namespace PDFresume
                 Address = "Rodriguez Rizal"
             };
 
+            string JsonOutput = JsonConvert.SerializeObject(personal);
+            txtBox1.Text = JsonOutput;
         }
     }
 }
